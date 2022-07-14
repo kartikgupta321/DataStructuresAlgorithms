fc4a63781846471c92e8a819998e2e4c56176d63
kartikgupta <kartik.gupta2505@gmail.com>
2022-07-14 12:28:33 +0530

    daily challenge

--- coding platform questions/leetcode questions/daily challenge/july/bt_right_side_view.java
+++ coding platform questions/leetcode questions/daily challenge/july/bt_right_side_view.java
@@ -1,39 +1,39 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-public class bt_right_side_view {
-    public static void main(String[] args) {
-        TreeNode p = new TreeNode(1);
-        p.left = new TreeNode(2);
-        p.left.left = new TreeNode(5);
-        p.left.left.right = new TreeNode(6);
-        p.right = new TreeNode(3);
-        // p.right.left = new TreeNode(6);
-        p.right.right = new TreeNode(4);
-        Solution obj = new Solution();
-        List<Integer> list = obj.rightSideView(p);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i)+" ");
-        }
-    }
-}
-class Solution {
-    List<Integer> list = new ArrayList<>();
-    public List<Integer> rightSideView(TreeNode root) {
-        if(root==null) return list;
-        Queue<TreeNode> que = new LinkedList<>();
-        que.add(root);
-        while(!que.isEmpty()){
-            int qlen = que.size();
-            TreeNode curr= new TreeNode();
-            for (int i = 0; i < qlen; i++) {
-                curr = que.poll();
-                if(curr.left!=null) que.add(curr.left);
-                if(curr.right!=null) que.add(curr.right);
-            }
-            list.add(curr.val);
-        }
-        return list;
-    }
-}
+// import java.util.ArrayList;
+// import java.util.LinkedList;
+// import java.util.List;
+// import java.util.Queue;
+// public class bt_right_side_view {
+//     public static void main(String[] args) {
+//         TreeNode p = new TreeNode(1);
+//         p.left = new TreeNode(2);
+//         p.left.left = new TreeNode(5);
+//         p.left.left.right = new TreeNode(6);
+//         p.right = new TreeNode(3);
+//         // p.right.left = new TreeNode(6);
+//         p.right.right = new TreeNode(4);
+//         Solution obj = new Solution();
+//         List<Integer> list = obj.rightSideView(p);
+//         for (int i = 0; i < list.size(); i++) {
+//             System.out.print(list.get(i)+" ");
+//         }
+//     }
+// }
+// class Solution {
+//     List<Integer> list = new ArrayList<>();
+//     public List<Integer> rightSideView(TreeNode root) {
+//         if(root==null) return list;
+//         Queue<TreeNode> que = new LinkedList<>();
+//         que.add(root);
+//         while(!que.isEmpty()){
+//             int qlen = que.size();
+//             TreeNode curr= new TreeNode();
+//             for (int i = 0; i < qlen; i++) {
+//                 curr = que.poll();
+//                 if(curr.left!=null) que.add(curr.left);
+//                 if(curr.right!=null) que.add(curr.right);
+//             }
+//             list.add(curr.val);
+//         }
+//         return list;
+//     }
+// }
--- coding platform questions/leetcode questions/tree/bt_levelorder.java
+++ coding platform questions/leetcode questions/tree/bt_levelorder.java
@@ -22,20 +22,40 @@ public class bt_levelorder {
         }
     }
 }
+// class Solution {
+//     List<List<Integer>> list = new ArrayList<>();
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         if(root==null) return list;
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+//         while (!queue.isEmpty()) {
+//             int qlen = queue.size();
+//             List<Integer> row = new ArrayList<>();
+//             for (int i = 0; i < qlen; i++) {
+//                 TreeNode curr = queue.poll();
+//                 row.add(curr.val);
+//                 if(curr.left != null) queue.add(curr.left);
+//                 if(curr.right != null) queue.add(curr.right);
+//             }
+//             list.add(row);
+//         }
+//         return list;
+//     }
+// }
 class Solution {
     List<List<Integer>> list = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
         if(root==null) return list;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            int qlen = queue.size();
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
+        while (!que.isEmpty()) {
+            int qlen = que.size();
             List<Integer> row = new ArrayList<>();
             for (int i = 0; i < qlen; i++) {
-                TreeNode curr = queue.poll();
+                TreeNode curr = que.poll();
                 row.add(curr.val);
-                if(curr.left != null) queue.add(curr.left);
-                if(curr.right != null) queue.add(curr.right);
+                if(curr.left!=null) que.add(curr.left);
+                if(curr.right!=null) que.add(curr.right);
             }
             list.add(row);
         }
