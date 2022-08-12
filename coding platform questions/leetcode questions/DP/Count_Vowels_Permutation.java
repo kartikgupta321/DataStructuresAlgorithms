db9c34a329ecb83d7095a99cc36e6c953a3f86ab
kartikgupta <kartik.gupta2505@gmail.com>
2022-08-12 08:08:15 +0530

    daily challenge august

--- coding platform questions/leetcode questions/DP/Count_Vowels_Permutation.java
+++ coding platform questions/leetcode questions/DP/Count_Vowels_Permutation.java
@@ -1,21 +1,30 @@
 public class Count_Vowels_Permutation {
     public static void main(String[] args) {
         Solution obj = new Solution();
-        System.out.println(obj.countVowelPermutation(1));
+        System.out.println(obj.countVowelPermutation(3000));
     }
 }
 class Solution {
     public int countVowelPermutation(int n) {
-        int dp[][]=new int [5][n];
+        int mod =  1000000007;
+        Long dp[][]=new Long [5][n];
         for (int i = 0; i < 5; i++) {
-            dp[i][0] = i;
+            dp[i][0] =  (long) 1;
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(dp[i][j]);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 1;   j < n; j++) {
+                dp[0][j] = (dp[1][j-1] + dp[2][j-1] + dp[4][j-1]) % mod;
+                dp[1][j] = (dp[0][j-1] + dp[2][j-1]) % mod ;
+                dp[2][j] = (dp[1][j-1] + dp[3][j-1]) % mod;
+                dp[3][j] = dp[2][j-1];
+                dp[4][j] = (dp[2][j-1] + dp[3][j-1]) % mod;
             }
-            System.out.println();
         }
-        return 0;
+        int sum = 0;
+        for (int j = 0; j < 5  ; j++) {
+            sum += dp[j][n-1] ;
+            sum = sum %mod;
+        }
+        return  sum%mod;
     }
 }
